535624215dcba29e7d0f3304607d7dc5a31e3ec0
Guillaume Coutiere <guillaume.coutiere@dindon.polytechnique.fr>
2024-05-27 18:19:00 +0200

    update

---
+++ data/dataset_generators/multi_situation.py
@@ -0,0 +1,89 @@
+from .base import DatasetGenerator
+import random
+
+def read_options(file, path="/users/eleves-b/2022/guillaume.coutiere/MODAL/MODAL_INF473/data/dataset_generators/options/", n=-1):
+    with open(path+file, 'r') as f:
+        options = f.readlines()
+        options = [option.strip() for option in options]
+        options = [option.split("_") if len(option.split("_")) > 1 else [option, "1"] for option in options]
+        options.sort(key=lambda option : float(option[-1]), reverse=True)
+    return options[:n]
+
+
+class MultiSituationPromptsDatasetGenerator(DatasetGenerator):
+    def __init__(
+        self,
+        generator,
+        batch_size=1,
+        output_dir="dataset/train",
+        num_images_per_label=200,
+    ):
+        super().__init__(generator, batch_size, output_dir)
+        self.num_images_per_label = num_images_per_label
+
+    def create_prompts_V1(self, labels_names):
+        prompts = {}
+        adjectives = read_options("adjectives.txt", n=2)
+        nature = read_options("nature.txt", n=1)
+        quantity = read_options("quantity.txt", n=4)
+        cheese_adjectives = read_options("cheese_adjectives.txt", n=3)
+        situations = read_options("situations.txt", n=8)
+
+        for label in labels_names:
+            prompts[label] = []
+            for adj, n_adj in adjectives:
+                n_adj = int(n_adj)
+                for nat, n_nat in nature:
+                    n_nat = int(n_nat)
+                    for qty, n_qty in quantity:
+                        n_qty = int(n_qty)
+                        for cadj, n_cadj in cheese_adjectives:
+                            n_cadj = int(n_cadj)
+                            for sit, n_sit in situations:
+                                n_sit = int(n_sit)
+                                n = n_adj*n_nat*n_qty*n_cadj*n_sit//10000
+                                prompts[label].append(
+                                    {
+                                        "prompt": f"{adj} {nat} of {qty} {cadj} {label} cheese {sit}",
+                                        "num_images": n,
+                                    }
+                                )
+        return prompts
+    
+
+    #cette fois-ci, on choisit au hasard un prompt en fonction de la pondération
+    def create_prompts(self, labels_names):
+        prompts = {}
+        adjectives = read_options("adjectives.txt", n=2)
+        nature = read_options("nature.txt", n=1)
+        quantity = read_options("quantity.txt", n=4)
+        cheese_adjectives = read_options("cheese_adjectives.txt", n=3)
+        situations = read_options("situations.txt", n=8)
+
+        adj, n_adj = zip(*adjectives)
+        nat, n_nat = zip(*nature)
+        qty, n_qty = zip(*quantity)
+        cadj, n_cadj = zip(*cheese_adjectives)
+        sit, n_sit = zip(*situations)
+        adj_weigths = [int(x) for x in n_adj]
+        nat_weigths = [int(x) for x in n_nat]
+        qty_weigths = [int(x) for x in n_qty]
+        cadj_weigths = [int(x) for x in n_cadj]
+        sit_weigths = [int(x) for x in n_sit]
+
+        for label in labels_names:
+            prompts[label] = []
+
+            for i in range(self.num_images_per_label):
+                adje = random.choices(adj, adj_weigths, k=1)[0]
+                natu = random.choices(nat, nat_weigths, k=1)[0]
+                qnty = random.choices(qty, qty_weigths, k=1)[0]
+                chadj = random.choices(cadj, cadj_weigths, k=1)[0]
+                situ = random.choices(sit, sit_weigths, k=1)[0]
+                prompts[label].append(
+                                    {
+                                        "prompt": f"{adje} {natu} of {qnty} {chadj} {label} cheese {situ}",
+                                        "num_images": 1,
+                                    } 
+                )
+        return prompts
---
+++ generate_script.py
@@ -0,0 +1,45 @@
+import os
+from diffusers import DiffusionPipeline, UNet2DConditionModel, DDIMScheduler
+from transformers import CLIPTextModel
+import torch
+from data.dataset_generators.multi_situation import MultiSituationPromptsDatasetGenerator
+
+checkpoints = [
+    200,
+    400,
+    600,
+    800,
+    1000,
+    1200,
+    1400,
+]
+
+fromages = os.listdir("/Data/guillaume.coutiere/finetune_data")
+
+dataset_generator = MultiSituationPromptsDatasetGenerator(None, 1, None, 400)
+
+for fromage in fromages[1:]:
+    for chk in checkpoints:
+
+        prompts = dataset_generator.create_prompts([fromage])
+        prompts = prompts[fromage]
+        prompts = [prompt["prompt"] for prompt in prompts]
+
+        scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
+
+        unet = UNet2DConditionModel.from_pretrained(f"/Data/guillaume.coutiere/models/image_generators/custom-sdxl-1-5/{fromage}/checkpoint-{chk}/unet")
+
+        # if you have trained with `--args.train_text_encoder` make sure to also load the text encoder
+        text_encoder = CLIPTextModel.from_pretrained(f"/Data/guillaume.coutiere/models/image_generators/custom-sdxl-1-5/{fromage}/checkpoint-{chk}/text_encoder")
+
+        pipeline = DiffusionPipeline.from_pretrained(
+                    "runwayml/stable-diffusion-v1-5", unet=unet, text_encoder=text_encoder, dtype=torch.float16, scheduler=scheduler
+                    ).to("cuda")
+        
+        for i, prompt in enumerate(prompts):
+            image = pipeline(prompt, num_inference_steps=100, guidance_scale=7.5).images[0]
+            image.save(f"/Data/guillaume.coutiere/photos_generated/{fromage}/{chk}_{i}_{prompt}.png")
+        
+        del pipeline
+        torch.cuda.empty_cache()
+
